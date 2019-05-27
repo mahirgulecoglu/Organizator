b85b2cdcede6547bb84dfc04425444f59d80e7cc
mahir.gulecoglu <mahir.gulecoglu@BK-LAB204-IS025>
2019-05-27 14:35:45 +0300

    sc

---
+++ Organizator/Organizator.DAL/Categories.cs
@@ -0,0 +1,27 @@
+namespace Organizator.DAL
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Categories
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Categories()
+        {
+            Event = new HashSet<Event>();
+        }
+
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Event> Event { get; set; }
+    }
+}
---
+++ Organizator/Organizator.DAL/DataContext.cs
@@ -0,0 +1,46 @@
+namespace Organizator.DAL
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class DataContext : DbContext
+    {
+        public DataContext()
+            : base("name=DataContext")
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Event> Event { get; set; }
+        public virtual DbSet<Message> Message { get; set; }
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<sysdiagrams> sysdiagrams { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>()
+                .HasMany(e => e.Event)
+                .WithRequired(e => e.Categories)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Person>()
+                .HasMany(e => e.Event)
+                .WithRequired(e => e.Person)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Person>()
+                .HasMany(e => e.Message)
+                .WithRequired(e => e.Person)
+                .HasForeignKey(e => e.MessageBy)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<Person>()
+                .HasMany(e => e.Message1)
+                .WithRequired(e => e.Person1)
+                .HasForeignKey(e => e.MessageTo)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
