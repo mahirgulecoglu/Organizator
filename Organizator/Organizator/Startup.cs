e85187feb6e2865d2280ec6dc28335cb8a432ca5
mahirgulecoglu <mahirgulecoglu@hotmail.com>
2019-05-27 13:51:46 +0300

    fc

---
+++ Organizator/Organizator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Organizator.Startup))]
+namespace Organizator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
