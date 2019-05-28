a1b49afafc3de4030bf7e6a6cbf75d2cea6272c9
Mahir Gulecoglu <mahirgulecoglu@hotmail.com>
2019-05-28 21:32:21 +0300

    add

---
+++ OrganizatorWEB/Organizator.BLL/EventBLL.cs
@@ -0,0 +1,20 @@
+﻿using Organizator.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organizator.BLL
+{
+    public class EventBLL
+    {
+        DataContext db = new DataContext();
+
+        public void AddEvent(Event events)
+        {
+            db.Event.Add(events);
+            db.SaveChanges();
+        }
+    }
+}
---
+++ OrganizatorWEB/Organizator.UI/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using Organizator.BLL;
+using Organizator.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Organizator.UI.Controllers
+{
+    public class EventController : Controller
+    {
+        EventBLL eventBLL = new EventBLL();
+        // GET: Event
+        public ActionResult AddEvent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEvent(Event events)
+        {
+            eventBLL.AddEvent(events);
+            return RedirectToAction("Detail", "Event");
+        }
+    }
+}
